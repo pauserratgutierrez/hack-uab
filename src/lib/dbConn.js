4126b3dbf2a674e7eed818f0278bcf3b6ec94150
Pau Serrat Gutiérrez <pauserratgutierrez@gmail.com>
2024-05-18 12:08:43 +0200

    feat: Add mysql2 dependency and configure database connection
    The `package.json` and `package-lock.json` files were updated to include the `mysql2` dependency with version `^3.9.7`. Additionally, a new file `src/lib/dbConn.js` was added to configure the connection to the MySQL database using environment variables.
    
    Note: Please make sure to update the environment variables (`DB_HOST`, `DB_USER`, `DB_PASSWORD`, `DB_DATABASE`, `DB_PORT`) accordingly.

---
+++ src/lib/dbConn.js
@@ -0,0 +1,11 @@
+import mysql from 'mysql2/promise'
+
+const config = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT
+}
+
+export const connection = await mysql.createConnection(config)
