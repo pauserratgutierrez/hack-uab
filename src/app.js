bc5654e2b82a3c195839212a78205f39009aca60
geniiss <128940272+geniiss@users.noreply.github.com>
2024-05-19 12:00:02 +0200

    refactor: Improve logging statements in app.js
    Refactor the logging statements in app.js to include additional information such as the lot, bloc, dia, tempsRuta, distanciaRuta, and municipis. This change aims to enhance the clarity and understanding of the logged data.

--- src/app.js
+++ src/app.js
@@ -21,6 +21,22 @@ const main = async () => {
     console.log({ lot, bloc, dia, tempsRuta, distanciaRuta, municipis });
     //municipis: routes[i][j][k].elements
   }
+
+  for (let i = 0; i < data.length; ++i) {
+    const {lot, bloc, dia, tempsRuta, distanciaRuta} = data[i];
+    console.log(`lot: ${lot}, setmana: ${bloc}`);
+    console.log(`dia: ${dia}`);
+    console.log(`durada jornada: ${tempsRuta}`);
+    process.stdout.write('municipis: ')
+    for(let j = 0; j < data[i].municipis.length; ++j){
+      if(j > 0) process.stdout.write(' -> ');
+
+      const municipiInfo = data[i].municipis[j].municipiInfo;
+      process.stdout.write(municipiInfo);
+      
+    }
+    console.log('\n');
+  }
 }; 
 await main();
 
