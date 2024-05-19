56fe8b0d30b06c7f1a298f3b6a6a4a7727ac9ad5
geniiss <128940272+geniiss@users.noreply.github.com>
2024-05-19 11:36:54 +0200

    refactor: Optimize app.js by extracting and organizing data from computGlobal()

--- src/app.js
+++ src/app.js
@@ -3,7 +3,24 @@ import { computGlobal } from './lib/getRoutes.js';
 
 const main = async () => {
   const data = await computGlobal();
-  console.log(data);
+  for (let i = 0; i < data.length; ++i) {
+    const {lot, bloc, dia, tempsRuta, distanciaRuta} = data[i];
+    const municipis = [];
+    for(let j = 0; j < data[i].municipis.length; ++j){
+      // const municipiId = row.id;
+      // const municipiGeo = { latitude: row.geopoint.x, longitude: row.geopoint.y };
+      // const municipiInfo = `${row.municipi}, ${row.comarca}`;
+      // const pobTotalNum = row.pob_total_num;
+      // const estanciaMin = parseTime(row.estancia_min);
+      const { latitude, longitude } = data[i].municipis[j].municipiGeo;
+
+      const { municipiId, municipiInfo, pobTotalNum, estanciaMin} = data[i].municipis[j];
+      municipis.push({municipiId, latitude, longitude, municipiInfo, pobTotalNum, estanciaMin});
+      
+    }
+    console.log({ lot, bloc, dia, tempsRuta, distanciaRuta, municipis });
+    //municipis: routes[i][j][k].elements
+  }
 }; 
 await main();
 
