c452dbbcea4b2f5bab7c9eb6a719cefeaa25ed6f
Pau Serrat Gutiérrez <pauserratgutierrez@gmail.com>
2024-05-20 20:04:40 +0200

    refactor: Improve route formatting logic for better readability and efficiency

--- src/app.js
+++ src/app.js
@@ -1,18 +1,21 @@
 import { mainCalcularRuta } from './lib/routes/mainCalcularRuta.js';
 import { formatRouteDataJSON, formatRouteDataNice } from './lib/routes/formatData.js';
 
-const main = async () => {
+async function main() {
   const restingHours = 1;
   const marginHours = 0.5;
   const truckVel = 75; // Truck velocity (km/h)
   const maxNumMunicipisDia = 6; // Maximum number of municipalities to visit in a day
   const numLots = [2, 4, 5];
 
+  console.log(`Calculant la millor ruta...\nDescans: ${restingHours}h, Marge: ${marginHours}h, Velocitat mitjana: ${truckVel}km/h, Màxim municipis diari: ${maxNumMunicipisDia}, Lots carregats: ${numLots}`);
+
   const data = await mainCalcularRuta(restingHours, marginHours, truckVel, maxNumMunicipisDia, numLots);
+
   const dataJSON = formatRouteDataJSON(data);
   const dataNice = formatRouteDataNice(dataJSON);
 
-  console.log(JSON.stringify(dataJSON, null, 2)); // Returns the grouped data in JSON format
+  // console.log(JSON.stringify(dataJSON, null, 2)); // Returns the grouped data in JSON format
   console.log(dataNice); // Returns the grouped data in a nice format
 };
 
