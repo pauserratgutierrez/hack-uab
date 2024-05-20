0ccb6ffcc90426e2c327d00451c1b9839b5efc04
Pau Serrat Gutiérrez <pauserratgutierrez@gmail.com>
2024-05-20 17:50:02 +0200

    refactor: Extract and organize data from computGlobal() in app.js
    Refactor the main function in app.js to extract and organize data from the computGlobal() function. This change improves the readability and maintainability of the code by separating the data extraction logic into a separate function. It also enhances the overall efficiency of the code by reducing unnecessary iterations and improving data handling.

--- src/app.js
+++ src/app.js
@@ -1,44 +1,48 @@
-// import { addMunicipiGeoDB } from './lib/database/dbUtils.js';
-import { computGlobal } from './lib/getRoutes.js';
+import { calcularRuta, formatRouteDataJSON, formatOutputNicely } from './lib/getRoutes.js';
+
+// ANTERIOR
+// const main = async () => {
+//   const data = await computGlobal();
+//   for (let i = 0; i < data.length; ++i) {
+//     const {lot, bloc, dia, tempsRuta, distanciaRuta} = data[i];
+//     const municipis = [];
+//     for(let j = 0; j < data[i].municipis.length; ++j){
+//       const { latitude, longitude } = data[i].municipis[j].municipiGeo;
+//       const { municipiId, municipiInfo, pobTotalNum, estanciaMin} = data[i].municipis[j];
+//       municipis.push({municipiId, latitude, longitude, municipiInfo, pobTotalNum, estanciaMin});
+//     }
+//     console.log({ lot, bloc, dia, tempsRuta, distanciaRuta, municipis });
+//   }
+
+//   for (let i = 0; i < data.length; ++i) {
+//     const {lot, bloc, dia, tempsRuta, distanciaRuta} = data[i];
+//     console.log(`lot: ${lot}, setmana: ${bloc}`);
+//     console.log(`dia: ${dia}`);
+//     console.log(`durada jornada: ${tempsRuta}`);
+//     process.stdout.write('municipis: ')
+//     for(let j = 0; j < data[i].municipis.length; ++j){
+//       if(j > 0) process.stdout.write(' -> ');
+
+//       const municipiInfo = data[i].municipis[j].municipiInfo;
+//       process.stdout.write(municipiInfo);
+      
+//     }
+//     console.log('\n');
+//   }
+// };
+
+// await main().then(() => {
+//   process.exit(0);
+// });
 
 const main = async () => {
-  const data = await computGlobal();
-  for (let i = 0; i < data.length; ++i) {
-    const {lot, bloc, dia, tempsRuta, distanciaRuta} = data[i];
-    const municipis = [];
-    for(let j = 0; j < data[i].municipis.length; ++j){
-      // const municipiId = row.id;
-      // const municipiGeo = { latitude: row.geopoint.x, longitude: row.geopoint.y };
-      // const municipiInfo = `${row.municipi}, ${row.comarca}`;
-      // const pobTotalNum = row.pob_total_num;
-      // const estanciaMin = parseTime(row.estancia_min);
-      const { latitude, longitude } = data[i].municipis[j].municipiGeo;
+  const dataJSON = await calcularRuta();
+  const dataNice = formatRouteDataJSON(dataJSON);
 
-      const { municipiId, municipiInfo, pobTotalNum, estanciaMin} = data[i].municipis[j];
-      municipis.push({municipiId, latitude, longitude, municipiInfo, pobTotalNum, estanciaMin});
-      
-    }
-    console.log({ lot, bloc, dia, tempsRuta, distanciaRuta, municipis });
-    //municipis: routes[i][j][k].elements
-  }
+  console.log(JSON.stringify(dataJSON, null, 2)); // Returns the grouped data in JSON format
+  console.log(formatOutputNicely(dataNice)); // Returns the grouped data in a nice format 
+};
 
-  for (let i = 0; i < data.length; ++i) {
-    const {lot, bloc, dia, tempsRuta, distanciaRuta} = data[i];
-    console.log(`lot: ${lot}, setmana: ${bloc}`);
-    console.log(`dia: ${dia}`);
-    console.log(`durada jornada: ${tempsRuta}`);
-    process.stdout.write('municipis: ')
-    for(let j = 0; j < data[i].municipis.length; ++j){
-      if(j > 0) process.stdout.write(' -> ');
-
-      const municipiInfo = data[i].municipis[j].municipiInfo;
-      process.stdout.write(municipiInfo);
-      
-    }
-    console.log('\n');
-  }
-}; 
-await main();
-
-// Acabem el programa
-process.exit(0);
+await main().then(() => {
+  process.exit(0);
+});
--- src/lib/getRoutes.js
+++ src/lib/getRoutes.js
@@ -1,49 +1,57 @@
 import { getMatrix } from './mapsAPI.js';
-import { addCacheDB, getCacheDB, getStartingPointsDB } from './database/dbUtils.js';
-import { getMunicipisGeoOrderedByDistanceDB } from './database/dbUtils.js';
-import { getMunicipisByLotsAndBlocsDB } from './database/dbUtils.js';
+import { addCacheDB, getCacheDB, getStartingPointsDB, getMunicipisGeoOrderedByDistanceDB, getMunicipisByLotsAndBlocsDB } from './database/dbUtils.js';
 
-export const computGlobal = async () => {
+export const calcularRuta = async () => {
+  // Personalize some values for the specific execution case
   const restingHours = 1;
   const marginHours = 0.5;
   const workingHours = 8 - restingHours - marginHours;
-
-  const truckVel = 75; // km/h
-  const maxNumMunicipisDia = 9; // posar num municipis
-
+  const truckVel = 75; // Truck velocity (km/h)
+  const maxNumMunicipisDia = 9; // Maximum number of municipalities to visit in a day
   const numLots = [2, 4, 5];
 
-  let abort = false;
+  let abort = false; // Flag to abort the recursive function
+  const distanceCache = await getCacheDB(); // Load the cache from the db in memory
 
-  const distanceCache = await getCacheDB();
-            
+  // ANTERIOR
+  // Load the municipalities of each lot and bloc
+  // const carregaLots = async () => {
+  //   const lots = [];
+  //   for (let i = 0; i < numLots.length; ++i) {
+  //     lots.push([]);
+  //     for (let j = 1; j <= 4; ++j) {
+  //       const mun = await getMunicipisByLotsAndBlocsDB(numLots[i], j); // Get the municipalities of the log and block from the database
+  //       lots[i].push(mun); // Save the municipalities in the array in memory
+  //     };
+  //   };
+  //   return lots; // Return the array with the municipalities
+  // };
+
   const carregaLots = async () => {
-    const lots = [];
-      for (let i = 0; i < numLots.length; ++i) {
-        lots.push([]);
-        for (let j = 1; j <= 4; ++j) {
-          const mun = await getMunicipisByLotsAndBlocsDB(numLots[i], j);
-          lots[i].push(mun);
-        }
+    return Promise.all(numLots.map(async (lot) => {
+      const blocs = [];
+      for (let bloc = 1; bloc <= 4; ++bloc) {
+        const municipis = await getMunicipisByLotsAndBlocsDB(lot, bloc);
+        blocs.push(municipis);
       }
-      return lots;
+      return blocs;
+    }));
   };
 
+  // BACK TRACKING ALGORITHM
+  // Recursive function to calculate the optimal route for each day of the week and each block of each lot taking into account the restrictions
   const computeRoute = async (i, j, notVisited, startingPoint, currentPoint, currentTime, workingHours, truckVel, path) => {
-    console.log(currentTime);
-
     const getDistance = async (origin, destination) => {
+      // Check if the distance is already in the cache and get it. If not, call the Google Maps API to get it and save it in the cache
+      // This way we avoid calling the Google Maps API multiple times for the same distance, which saves time and money
       const key1 = `${origin}-${destination}`;
-      const key2 = `${destination}-${origin}`;
-      if (distanceCache.has(key1)) {
-        return distanceCache.get(key1);
-      }
-        else if (distanceCache.has(key2)) {
-          return distanceCache.get(key2);
-        }
-        const result = await getMatrix([origin], [destination]);
-        distanceCache.set(key1, result.distance);
-        return result.distance;
+      const key2 = `${destination}-${origin}`; // A->B and B->A, so we don't miss any distance
+      if (distanceCache.has(key1)) return distanceCache.get(key1);
+      if (distanceCache.has(key2)) return distanceCache.get(key2);
+
+      const result = await getMatrix([origin], [destination]); // Call the Google Maps API to get the distances between the two points
+      distanceCache.set(key1, result.distance); // Save the distance in the cache
+      return result.distance;
       }
 
       const distanceInfo = await getDistance(currentPoint.municipiInfo, startingPoint.municipiInfo);
@@ -58,8 +66,8 @@ export const computGlobal = async () => {
         if (notVisited.has(nearby[l].municipiInfo)) {
           const toNearbyDistance = await getDistance(currentPoint.municipiInfo, nearby[l].municipiInfo);
           const backToStartDistance = await getDistance(nearby[l].municipiInfo, startingPoint.municipiInfo);
-          console.log(`${currentPoint.municipiInfo} -> ${nearby[l].municipiInfo} (${Math.round(toNearbyDistance)}km)`);
-          console.log(`${nearby[l].municipiInfo} -> ${startingPoint.municipiInfo} (${Math.round(backToStartDistance)}km)`);
+          // console.log(`${currentPoint.municipiInfo} -> ${nearby[l].municipiInfo} (${Math.round(toNearbyDistance)}km)`);
+          // console.log(`${nearby[l].municipiInfo} -> ${startingPoint.municipiInfo} (${Math.round(backToStartDistance)}km)`);
 
           const totalTime = parseFloat(currentTime + toNearbyDistance/truckVel + backToStartDistance/truckVel);
           if (totalTime > workingHours) {
@@ -123,10 +131,48 @@ export const computGlobal = async () => {
 
   const startingPoints = await getStartingPointsDB();
   const lots = await carregaLots();
-  const result = await getRoutes(lots, startingPoints, workingHours, truckVel); 
-  console.log("result done");
-
+  const result = await getRoutes(lots, startingPoints, workingHours, truckVel);
   await addCacheDB(distanceCache);
 
   return result;
+};
+
+// Formats the data in JSON format
+export const formatRouteDataJSON = (data) => {
+  const groupedData = {};
+  data.forEach(({ lot, bloc, dia, tempsRuta, municipis }) => {
+    if (!groupedData[lot]) groupedData[lot] = {};
+    if (!groupedData[lot][bloc]) groupedData[lot][bloc] = {};
+    if (!groupedData[lot][bloc][dia]) groupedData[lot][bloc][dia] = [];
+    const detailedMunicipis = municipis.map(({ municipiGeo: { latitude, longitude }, municipiId, municipiInfo, pobTotalNum, estanciaMin }) => ({
+      municipiId,
+      municipiGeo: { latitude, longitude },
+      municipiInfo,
+      pobTotalNum,
+      estanciaMin
+    }));
+    groupedData[lot][bloc][dia].push({ tempsRuta, municipis: detailedMunicipis });
+  });
+
+  return groupedData;
+};
+
+// Formats the data in a more readable way suitable for a console output
+export const formatOutputNicely = (data) => {
+  const output = [];
+
+  for (const lot in data) {
+    output.push(`LOT: ${lot}`);
+    for (const bloc in data[lot]) {
+      output.push(`Setmana ${bloc}`);
+      for (const dia in data[lot][bloc]) {
+        data[lot][bloc][dia].forEach(({ tempsRuta, municipis }) => {
+          const ruta = municipis.map(m => m.municipiInfo).join(' -> ');
+          output.push(`Dia ${parseInt(dia) + 1} -> Jornada: ${tempsRuta.toFixed(2)}h | Ruta: ${ruta}`);
+        });
+      }
+    }
+  }
+
+  return output.join('\n');
 };
